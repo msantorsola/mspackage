309b6652128aeb83bc2a2d2cf18c95df0c3aedb1
Mariangela Santorsola <ma.santorsola@gmail.com>
2024-05-03 10:49:27 +0000

    added license

--- tests/testthat/test-make_scatterplot.R
+++ tests/testthat/test-make_scatterplot.R
@@ -1,6 +1,5 @@
 test_that("multiplication works", {
-  tsv_file <- system.file("extdata", "dataset.tsv", package = "packagetraining")
-  scatterplot <- make_scatterplot(tsv_file)
-  # expect_that("ggplot" %in% class(scatterplot), equals(TRUE)) # deprecated
-  expect_true("ggplot" %in% class(scatterplot), equals(TRUE))
+  tsv_file <- system.file("extdata",  "example_dataset.tsv", package = "mspackage")
+  scatterplot = make_scatterplot(tsv_file)
+  expect_that("ggplot" %in% class(scatterplot), equals(TRUE))
 })
--- tests/testthat/test-summarise_dataset.R
+++ tests/testthat/test-summarise_dataset.R
@@ -1,3 +1,5 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("summarise_data works", {
+  tsv_file <- system.file("extdata", "example_dataset.tsv", package = "packagetraining")
+  summary <- summarise_dataset(tsv_file)
+  expect_equal(dim(summary)[[2]], 4)
 })
